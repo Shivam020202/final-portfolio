d7c7cde4f79985e2bd0e46c28161c65b429cee4c
Shivam020202 <charlieshivam70@gmail.com>
2025-12-10 15:38:05 +0530

    works added

--- src/app/components/Works.jsx
+++ src/app/components/Works.jsx
@@ -25,11 +25,11 @@ const Works = () => {
         src="https://res.cloudinary.com/dsgolax64/image/upload/v1750422186/image_12_y7npm0.png"
         width="1000"
         height="1000"
-        alt="men of culture website preview"
+        alt="grb tests preview"
         className="w-full rounded-lg object-cover  border-2 border-[#1c1c1c] h-auto"
       />
       <p className="bg-black border-2 border-[#1c1c1c] w-full p-2 rounded-lg mt-4 text-sm text-gray-500">
-        Note - Yet to go live -
+        Live on -
         <Link
           className="text-blue-600 ml-2 hover:text-blue-400 hover:underline duration-300 transition-colors animate-bounce"
           href="www.grbtests.com"
@@ -37,6 +37,36 @@ const Works = () => {
           www.grbtests.com
         </Link>
       </p>
+
+      <h2 className="italic mb-4  mt-6">Yogek Website</h2>
+
+      <Image
+        src="https://res.cloudinary.com/dsgolax64/image/upload/v1765360520/3a35bb10-61e2-4fcb-91e0-a49add982681.png"
+        width="1000"
+        height="1000"
+        alt="yogek website preview"
+        className="w-full rounded-lg object-cover  border-2 border-[#1c1c1c] h-auto"
+      />
+      <p className="bg-black border-2 border-[#1c1c1c] w-full p-2 rounded-lg mt-4 text-sm text-gray-500">
+        Live on -
+        <Link
+          className="text-blue-600 ml-2 hover:text-blue-400 hover:underline duration-300 transition-colors animate-bounce"
+          href="www.yogikmovement.com"
+        >
+          www.yogikmovement.com
+        </Link>
+      </p>
+      <h2 className="italic mb-4  mt-6">
+        Thapar Institute Recruitment Dashboard{" "}
+      </h2>
+
+      <Image
+        src="https://res.cloudinary.com/dsgolax64/image/upload/v1765360778/0f99ccde-5553-4da8-a8d0-702a252b62fe.png"
+        width="1000"
+        height="1000"
+        alt="thapar institute recruitment dashboard preview"
+        className="w-full rounded-lg object-cover  border-2 border-[#1c1c1c] h-auto"
+      />
     </div>
   );
 };
